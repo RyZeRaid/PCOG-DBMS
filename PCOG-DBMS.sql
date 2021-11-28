1aa76690d8f959c96b77e2688702a5ef90a15383
Onandi Skeen <onandiskeen@gmail.com>
2021-11-28 17:32:42 -0500

    Added files

---
+++ PCOG-DBMS.sql
@@ -0,0 +1,58 @@
+DROP DATABASE IF EXISTS ChurchofGoddb;
+CREATE DATABASE ChurchofGoddb;
+USE ChurchofGoddb;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+    `id` INT unsigned AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL ,
+    `password` VARCHAR(255) NOT NULL ,
+    `date_created` DATETIME NOT NULL ,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+    `id` INT unsigned AUTO_INCREMENT,
+    `firstname` VARCHAR(255) NOT NULL ,
+    `lasttname` VARCHAR(255) NOT NULL ,
+    `age` INT(3) NOT NULL ,
+    `gender` VARCHAR(8) NOT NULL ,
+    `position` VARCHAR(255) NOT NULL ,
+    `home_address` VARCHAR(255) NOT NULL ,
+    `email` VARCHAR(50) NOT NULL ,
+    `phonenumber` INT(50) NOT NULL ,
+    `date_of_birth` DATETIME NOT NULL ,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4080 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `priority1`;
+CREATE TABLE `priority1` (
+    `id` INT(11) NOT NULL,
+    `firstname` VARCHAR(255) NOT NULL ,
+    `lasttname` VARCHAR(255) NOT NULL ,
+    `position` VARCHAR(50) NOT NULL ,
+    `priority` VARCHAR(12) NOT NULL,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `priority2`;
+CREATE TABLE `priority2` (
+    `id` INT(11) NOT NULL,
+    `firstname` VARCHAR(255) NOT NULL ,
+    `lasttname` VARCHAR(255) NOT NULL ,
+    `position` VARCHAR(50) NOT NULL ,
+    `priority` VARCHAR(12) NOT NULL,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `attendance`;
+CREATE TABLE `attendance` (
+    `id` INT(11) NOT NULL,
+    `firstname` VARCHAR(255) NOT NULL ,
+    `lasttname` VARCHAR(255) NOT NULL ,
+    `date` DATETIME NOT NULL ,
+    PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/* GRANT ALL PRIVILEGES ON user.* TO 'new_user'@'localhost'IDENTIFIED BY 'password123';*/
---
+++ login.js
@@ -0,0 +1,47 @@
+window.onload = function(){
+
+    let login = document.querySelector("#btn");
+    let error = document.querySelector(".error")
+    let error1 = document.querySelector(".error1")
+
+    login.addEventListener("click", onClick);
+
+    function onClick(e) {
+        let valid = 0;
+        
+        e.preventDefault();
+
+        let username = document.querySelector("#username").value
+        let password = document.querySelector("#password").value
+
+
+        if (username === ""){
+            error.innerHTML = "*Please enter username"
+        }else {
+            valid += 1;
+            error.innerHTML = ""
+        }
+
+        if (password === ""){
+            error1.innerHTML = "*Please enter password"
+        }else {
+            valid += 1;
+            error1.innerHTML = ""
+        }
+
+
+        if (valid == 2){
+            const xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200){
+                    document.getElementById("show").innerHTML = this.responseText;
+                }
+            }
+            xhr.open('GET', 'login.php?user='+ username + " " + password, true);
+            
+            xhr.send();
+        }
+
+    }
+}
