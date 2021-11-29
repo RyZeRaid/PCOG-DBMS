169a717ddcf657a93450bda432b740d52256b5e4
Ajani-Lewis <ajani.j.lewis@gmail.com>
2021-11-29 17:48:42 -0500

    Attempting to reupload

--- scripts/add-members.js
+++ scripts/add-members.js
@@ -10,8 +10,6 @@ window.onload=function(){
         numbers=/^[0-9]+$/ ;
         alpha2=/^[0-9A-Za-z]+$/;
         date=/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
-        emailcheck=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        let valid = 0;
         e.preventDefault();
         var fname=document.querySelector("#fname").value.trim();
         var lname=document.querySelector("#lname").value.trim();
@@ -23,17 +21,15 @@ window.onload=function(){
         var add2=document.querySelector("#add-line2").value.trim();
         var city=document.querySelector("#city").value.trim();
         var parish=document.querySelector("#parish").value;
-        var email=document.querySelector('#email').value.trim();
-        var phonenum=document.querySelector('#phonenum').value.trim();
+    
        
-        console.log(fname,lname,dob,age,gender,position,add1,add2,city,parish,email,phonenum);
+        console.log(fname,lname,dob,age,gender,position,add1,add2,city,parish);
 
         if (!fname.match(alpha)){
             document.querySelector("#fname").style.borderColor="red";
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#fname").style.borderColor="black";
         }
         if (!lname.match(alpha)){
@@ -41,7 +37,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#lname").style.borderColor="black";
         }
         if (!dob.match(date)){
@@ -49,7 +44,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#dob").style.borderColor="black";
         }
         if (!age.match(numbers)){
@@ -57,7 +51,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#age").style.borderColor="black";
         }
         if (!position.match(alpha)){
@@ -65,7 +58,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#position").style.borderColor="black";
         }
         if (!add1.match(alpha2)){
@@ -73,7 +65,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#add-line1").style.borderColor="black";
         }
         if (!add2.match(alpha2)){
@@ -81,7 +72,6 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#add-line2").style.borderColor="black";
         }
         if (!city.match(alpha2)){
@@ -89,45 +79,9 @@ window.onload=function(){
             return;
         }
         else{
-            valid = valid + 1;
             document.querySelector("#city").style.borderColor="black";
         }
-        if (!phonenum.match(numbers)){
-            document.querySelector("#phonenum").style.borderColor="red";
-            return;
-        }
-        else{
-            valid = valid + 1;
-            document.querySelector("#phonenum").style.borderColor="black";
-        }
-        if (!email.match(emailcheck)){
-            document.querySelector("#email").style.borderColor="red";
-            return;
-        }
-        else{
-            valid = valid + 1;
-            document.querySelector("#email").style.borderColor="black";
-        }
 
-        if (valid == 10){
-            const xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function(){
-
-
-                if(this.readyState == 4 && this.status == 200){
-                    document.getElementById("show").innerHTML = this.responseText;
-
-                    console.log(this.responseText)
-
-                    window.location.href = "HomeScreen.php";
-                    
-                }
-            }
-            xhr.open('GET', 'HomeScreen.php?member='+ fname + " " + lname + " " + dob + " " + age + " " + gender + " " + position + " " + add1 + " " + add2 + " " + city + " " + parish + " " + email + " " + phonenum, true);
-            
-            xhr.send();
-        }
 
     }
 }
