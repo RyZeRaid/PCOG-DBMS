ba6cc08726473c12159124e3c04ddbc6524a9eaf
Onandi Skeen <onandiskeen@gmail.com>
2021-11-30 01:57:25 -0500

    Added Search and Sort Funtionality

--- scripts/memberslist.js
+++ scripts/memberslist.js
@@ -17,6 +17,34 @@ window.onload = function(){
 
     function onClick(e){
         let response = document.querySelector("#text").value;
+        let radios = document.getElementsByName('f');
+
+
+        for (var i = 0, length = radios.length; i < length; i++) {
+            if (radios[i].checked) {
+                let option  = radios[i].id;  
+                
+                const xhr = new XMLHttpRequest();
+
+                xhr.onreadystatechange = function(){
+                    if(this.readyState == 4 && this.status == 200){
+                        document.getElementById("table").innerHTML = this.responseText;
+                    }
+                }
+                if(e.target.id === "search"){
+                    xhr.open('GET', 'members.php?member='+response+"&option="+option+"&context=search", true);
+                }else if (e.target.id === "sort"){
+                    xhr.open('GET', 'members.php?member='+response+"&option="+option+"&context=sort", true);
+                }
+                xhr.send();
+                return;
+            }
+        }
+
+        
+        if(e.target.id === "sort" && response === ""){
+            document.getElementById("table").innerHTML = "Please Select a Sort key";
+        }
 
         const xhr = new XMLHttpRequest();
 
@@ -25,10 +53,10 @@ window.onload = function(){
                 document.getElementById("table").innerHTML = this.responseText;
             }
         }
-        if(e.target.id === "search"){
+        if(e.target.id === "search" && response === ""){
             xhr.open('GET', 'members.php?member='+response+"&context", true);
-        }else if (e.target.id === "sort"){
-            //xhr.open('GET', 'world.php?country='+response+"&context=cities", true);
+        }else if (e.target.id === "search" && response !== "") {
+            document.getElementById("table").innerHTML = "Please Select a Search key";
         }
         
         xhr.send();
